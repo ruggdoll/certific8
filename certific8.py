21defa7b26d819ede82df96b56d633091e79dc4f
Deel <contact@ruggdoll.com>
2022-09-26 00:34:11 +0200

    Update certific8.py

--- certific8.py
+++ certific8.py
@@ -6,28 +6,26 @@ import json
 import argparse
 
 def fetch_domains(domain):
-	session = requests.session()
-	ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
-	headers = {'User-Agent': ua}
-	session.headers = headers
-	url = "https://crt.sh/?q="+domain+"{}&output=json&exclude=expired"
-	req = session.get(url)
-	data = list_cleanser(json.loads(req.content.decode('utf-8')))
-	return data
+    session = requests.session()
+    ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
+    headers = {'User-Agent': ua}
+    session.headers = headers
+    url = "https://crt.sh/?q="+domain+"&output=json&exclude=expired"
+    req = session.get(url)
+    data = list_cleanser(json.loads(req.content.decode('utf-8')))
+    return data
 
 def list_cleanser(rawlist):
-	mylist=[]
-	for item in rawlist:
-		if item["common_name"] not in mylist:
-			mylist.append(item["common_name"])
-	return(mylist)
+    mylist=[]
+    for item in rawlist:
+        if item["common_name"] not in mylist:
+            mylist.append(item["common_name"])
+    return(mylist)
 
 def ssl_expiry_datetime(hostname):
     ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
-
     context = ssl.create_default_context()
     context.check_hostname = False
-
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
         server_hostname=hostname,
@@ -50,6 +48,6 @@ if __name__ == "__main__":
         try:
             expire = ssl_expiry_datetime(subdomains)
             diff = expire - now
-            print ("{} Expiration_date: {} Remaining_days: {}".format(subdomains,expire.strftime("%d/%m/%Y"),diff.days))
-        except Exception as e:
+            print ("Domain:{} Expiration_date:{} Remaining_days:{}".format(subdomains,expire.strftime("%d/%m/%Y"),diff.days))        
+	except Exception as e:
             print (subdomains,"ERROR :",e)
